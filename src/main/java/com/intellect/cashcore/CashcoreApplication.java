aa4722cab279a472cc44d1fe6c70114e06cf1e13
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-11-30 18:15:39 +0530

    Extends SpringBootServletInitializer

--- src/main/java/com/intellect/cashcore/CashcoreApplication.java
+++ src/main/java/com/intellect/cashcore/CashcoreApplication.java
@@ -2,21 +2,20 @@ package com.intellect.cashcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.intellect.cashcor"})
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 public class CashcoreApplication extends SpringBootServletInitializer {
-	public static void main(String[] args) {
+	<!-- public static void main(String[] args) {
 		SpringApplication.run(CashcoreApplication.class, args);
-	}
+	} -->
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CashcoreApplication.class);
+    }
+	
 @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
       return builder.sources(CashcoreApplication.class);
