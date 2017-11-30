6eabd3ccd030288730b70633eb0b9c9b41791697
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-11-30 18:05:54 +0530

    update import class

--- src/main/java/com/intellect/cashcore/CashcoreApplication.java
+++ src/main/java/com/intellect/cashcore/CashcoreApplication.java
@@ -2,6 +2,7 @@ package com.intellect.cashcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
