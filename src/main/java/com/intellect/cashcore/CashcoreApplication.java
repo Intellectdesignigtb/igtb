3fe6908c1629ab1c4ac170a004a0391ecc498bc4
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-11-30 17:38:27 +0530

    update file

--- src/main/java/com/intellect/cashcore/CashcoreApplication.java
+++ src/main/java/com/intellect/cashcore/CashcoreApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CashcoreApplication {
+public class CashcoreApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CashcoreApplication.class, args);
 	}
+@Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(Application.class);
+  }
 }
