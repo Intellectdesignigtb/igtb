694f0d90cd7ce382dcd6c5b4311107295e5d81ec
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-11-30 17:49:17 +0530

    import class

--- src/main/java/com/intellect/cashcore/CashcoreApplication.java
+++ src/main/java/com/intellect/cashcore/CashcoreApplication.java
@@ -2,6 +2,7 @@ package com.intellect.cashcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CashcoreApplication extends SpringBootServletInitializer {
