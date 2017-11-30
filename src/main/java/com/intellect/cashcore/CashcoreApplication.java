159576fe5b6463866540b3163963687740058b87
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-11-30 17:57:35 +0530

    update

--- src/main/java/com/intellect/cashcore/CashcoreApplication.java
+++ src/main/java/com/intellect/cashcore/CashcoreApplication.java
@@ -3,8 +3,15 @@ package com.intellect.cashcore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.intellect.cashcor"}
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class CashcoreApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CashcoreApplication.class, args);
