1b95c51104e3fbfa1dc2e38511925d5d2daedc1f
ganeshmali50 <34073095+ganeshmali50@users.noreply.github.com>
2017-12-01 16:31:09 +0530

    add assertEquals

--- src/test/java/com/intellect/cashcore/CashcoreApplicationTests.java
+++ src/test/java/com/intellect/cashcore/CashcoreApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,8 @@ public class CashcoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	assertEquals(0, 0, "10 x 0 must be 0");
+		
 	}
 
 }
